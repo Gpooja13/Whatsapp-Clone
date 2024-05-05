ba8fdda779596f406f3ed2c17d0da2942ca3a078
Gpooja13 <145600866+Gpooja13@users.noreply.github.com>
2024-05-05 10:50:28 +0530

    added env

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_ZEGO_APP_ID: 1369726402,
     NEXT_PUBLIC_ZEGO_SERVER_ID: "13338a91e17fe69b02c0c8440d114d8d",
-    // NEXT_PUBLIC_HOST: "http://localhost:3005",
+    NEXT_PUBLIC_HOST:"https://whatsapp-clone-w9e6.onrender.com"
   },
   images: {
     domains: ["localhost"],
